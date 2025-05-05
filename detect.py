9f6cbcfcf925e79386dcd2fd7c8ae15f908d5638
Zhijing Xin <zhijingxin@stu.cqu.edu.cn>
2025-05-05 09:39:09 +0800

    fix detect bug

--- detect.py
+++ detect.py
@@ -72,7 +72,7 @@ def detect(save_img=False):
         pred = model(img, augment=opt.augment)[0]
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(pred[0], opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
         t2 = time_synchronized()
 
         # Apply Classifier
